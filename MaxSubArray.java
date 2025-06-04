88dd7b415d6df2a30eeb07a8fd8874e3b5b177f4
Rakshitha <118209233+Rakshithashun@users.noreply.github.com>
2025-06-04 14:58:57 +0530

    MaxSubArray

---
+++ MaxSubArray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0],sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum = Math.max(nums[i],sum+nums[i]);
+            max = Math.max(sum,max);
+        }
+        return max;
+    }
+}
