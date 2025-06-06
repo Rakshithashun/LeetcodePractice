5263292f191e2d741389044faaa739b34e38d569
Rakshitha <118209233+Rakshithashun@users.noreply.github.com>
2025-06-06 15:25:52 +0530

    ReverseOfInteger

---
+++ ReverseOfInteger.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+    long rev=0;
+      while(x!=0){
+         int rem=x%10;
+         rev=(rev*10)+rem;
+         x/=10;
+      }
+      if(rev<Integer.MIN_VALUE||rev>Integer.MAX_VALUE){
+        return 0;
+      }
+      else{
+        return (int) rev;
+      }
+    }
+}
